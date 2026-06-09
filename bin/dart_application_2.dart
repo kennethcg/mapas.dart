93a39825e92cfa75f8e61b1873eef6e10e7aabd7
Kenneth <kennethcogo@gmail.com>
2026-06-09 07:16:26 -0500

    Primer commit

---
+++ bin/dart_application_2.dart
@@ -0,0 +1,11 @@
+void main(List<String> arguments) {
+  Map<String, String> agenda = {
+    "cristian": "200000",
+    "kenneth": "200001",
+    "sebastian": "200342",
+  };
+
+  print("${agenda["cristian"]}");
+  print("${agenda["kenneth"]}");
+  print("${agenda["sebastian"]}");
+}
